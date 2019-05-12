dc127b246f4e60e5852bcf72e4e3c4b813f0801d
petergdoyle <peter.doyle@travelport.com>
2019-05-12 09:47:28 -0600

    removed pause

--- kafka_check_status.sh
+++ kafka_check_status.sh
@@ -3,13 +3,11 @@ source ./kafka_common.sh
 
 check_zookeeper_status
 echo -e "tail on zookeeper log(s) 5 seconds..."
-sleep1
 cmd="timeout 5s tail -f $KAFKA_HOME/logs/zookeeper-*-console.log"
 echo "$cmd"
 eval "$cmd"
 check_broker_status
 echo -e "tail on broker log(s) 5 seconds..."
-sleep1
 cmd="timeout 5s tail -f $KAFKA_HOME/logs/broker-*-console.log"
 echo "$cmd"
 eval "$cmd"
